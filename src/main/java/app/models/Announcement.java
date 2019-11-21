4f4e2ec402c5f0bf7007e4ef9f7418ffe2a9a12a
Gabriel Vander <gabsvander@hotmail.com>
2019-11-21 17:47:00 -0300

    Announcement Hibernate mapping

--- src/main/java/app/models/Announcement.java
+++ src/main/java/app/models/Announcement.java
@@ -1,6 +1,14 @@
 package app.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Announcement extends Document {
+  @Id
+  @GeneratedValue
+  private long id;
 
   private String description;
 
