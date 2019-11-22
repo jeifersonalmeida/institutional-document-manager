b31b946fd30d321c0fa2150b47a23cb3d1e441a9
Gabriel Vander <gabsvander@hotmail.com>
2019-11-21 22:18:45 -0300

    PublicServantDAO creation

---
+++ src/main/java/app/DAO/PublicServantDAO.java
@@ -0,0 +1,10 @@
+package app.DAO;
+
+import app.models.PublicServant;
+
+public class PublicServantDAO extends GenericDAO<PublicServant> {
+  public PublicServantDAO(){
+    super(PublicServant.class);
+  }
+  
+}
