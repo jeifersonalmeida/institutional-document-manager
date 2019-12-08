fe4494e5702290e16ba096b061e10107b8644d2e
Gabriel Vander <gabsvander@hotmail.com>
2019-12-08 19:01:07 -0300

    Bug fixes

--- src/main/java/app/models/utils/DateTransformer.java
+++ src/main/java/app/models/utils/DateTransformer.java
@@ -9,18 +9,18 @@ import java.util.List;
 public abstract class DateTransformer {
   private static final Map<String, Integer> months = new HashMap<String, Integer>(){
     {
-      put("JAN", 1);
-      put("FEV", 2);
-      put("MAR", 3);
-      put("ABR", 4);
-      put("MAI", 5);
-      put("JUN", 6);
-      put("JUL", 7);
-      put("AGO", 8);
-      put("SET", 9);
-      put("OUT", 10);
-      put("NOV", 11);
-      put("DEZ", 12);
+      put("JAN", 12);
+      put("FEV", 11);
+      put("MAR", 10);
+      put("ABR", 9);
+      put("MAI", 8);
+      put("JUN", 7);
+      put("JUL", 6);
+      put("AGO", 5);
+      put("SET", 4);
+      put("OUT", 3);
+      put("NOV", 2);
+      put("DEZ", 1);
     }
   };
 
@@ -51,7 +51,7 @@ public abstract class DateTransformer {
       properString.set(index, months.get(properString.get(index).substring(0, 3)).toString());
 
       List<String> list = new ArrayList<>();
-      list.add(properString.get(properString.size()-1));
+      list.add(properString.get(properString.size()-1).replace(".", ""));
       list.add(properString.get(index));
       list.add(properString.get(0));
 
